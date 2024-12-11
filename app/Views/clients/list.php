a06a7e864ac475e08ab16f43ae29857389e550ef
sergioalvespaeslopes <se_manoel@hotmail.com>
2024-12-11 16:19:28 -0300

    11_12_24

--- app/Views/clients/list.php
+++ app/Views/clients/list.php
@@ -6,6 +6,7 @@
 
 <?= $this->section('content'); ?>
 
+
 <div class="container mx-auto p-6 bg-white shadow-lg rounded-lg">
     <h1 class="text-2xl font-semibold text-gray-800 mb-6">Clientes</h1>
     <a href="<?= base_url('/clients/create'); ?>" class="inline-block bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 mb-6">Criar Novo Cliente</a>
--- app/Views/layouts/base.php
+++ app/Views/layouts/base.php
@@ -4,12 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= isset($title) ? esc($title) : 'Meu Site'; ?></title>
+
     <link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/jquery.dataTables.min.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <script src="https://cdn.tailwindcss.com"></script>
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <?= $this->renderSection('css'); ?>
+
+    <?= $this->renderSection('css'); ?> 
 </head>
 <body>
     <?= $this->include('layouts/header'); ?>
@@ -17,6 +19,6 @@
         <?= $this->renderSection('content'); ?>
     </main>
     <?= $this->include('layouts/footer'); ?>
-    <?= $this->renderSection('js'); ?> <!-- Para scripts adicionais -->
+    <?= $this->renderSection('js'); ?> 
 </body>
 </html>
